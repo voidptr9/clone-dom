c4954770fba443c8e248ab246a87c6d02c352d55
Saddam M <alimohamuda80@gmail.com>
2021-09-01 17:10:13 +0200

    Memory stats.

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ DOM element creation is a straightforward process. The browser calls the underly
 
 Sometimes, creating thousands of nodes can get very tricky and affect the memory complexity of your web application. This is very much notable in huge apps where massive tables or shared components are used.
 
+### Cloning identical nodes
+
 Luckily, with the `node.cloneNode()` method on elements, we can effectively use the shared attributes and define a `Record<SerializedProps, Element>` _relationship_ between any arbitrary nodes to ensure that, say, _X_ only yields _Y_ when it satisfies a single condition, **shared serializable props**.
 
 This means LazyDOM can effectively reduce the memory overhead of creating 10,000 similar nodes to, say, _N_, which depends on the **rate of identity matches** between nodes. The more the `<td />` elements with the attribute `class="cell center"`, the more the matches, hence more cloning over creation.
@@ -20,9 +22,27 @@ This is a sample benchmark demonstrating the performance of `cloneNode()` (±28.
 
 **Browser/OS:** Firefox 91 on Windows
 
-The biggest disadvantage is the register could be overpopulated with non-weak in-memory JavaScript objects. One advantage is LazyDOM does not deep copy children and only retains single element info. Additionally, only string props are checked against, meaning, event handlers will **always** be re-attached synchronously since such info is typically lost in the cloning process.
+### Register overpopulation
+
+A major risk would be that the register could be overpopulated with non-weak in-memory JavaScript objects. One advantage is LazyDOM does not deep copy children and only retains single element info. Additionally, only string props are checked against, meaning, event handlers will **always** be re-attached synchronously since such info is typically lost in the cloning process.
 
 Finally, for elements without any attributes (checked via `NamedNodeMap.prototype.length`), a `__no_attr__` check is used. So every `<p />` node without attributes hits said check and gets cloned immediately, children and event handlers attached, then returned.
+
+### RAM/CPU/Memory vs. Time complexity
+
+The test above was rather theoretical so I instead replicated a small environment where two suites, lazy and regular, are run subsequently after timeouts. Each suite creates a list of 100,000 `div` elements iteratively. Memory stats are visually displayed via [memory-stats](https://github.com/paulirish/memory-stats.js) and `console.time()`, `console.timeEnd()` and `console.memory` used respectively. Results:
+
+<img align="center" src="https://github.com/voidptr9/lazy-dom/blob/master/res/mem-stats.png?raw=true"></img>
+
+You might've noticed the memory-time relationship here:
+
+**Regular DOM has quicker creations**.
+
+**LazyDOM has consumes less memory**.
+
+## Missing features
+
+- Flushing the register after a specified timeout or size limit (via checking against `Object.keys(register).length`)
 
 ## What LazyDOM is not
 
--- test/index.ts
+++ test/index.ts
@@ -1,42 +1,97 @@
+// @ts-ignore
+import MemoryStats from "memory-stats";
 import LazyDOM from "../src";
 
-function getRandomColor() {
-  const letters = "0123456789ABCDEF";
-  let color = "#";
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
+const stats = new MemoryStats();
+stats.domElement.style.position = "fixed";
+stats.domElement.style.right = "0px";
+stats.domElement.style.top = "0px";
+document.body.appendChild(stats.domElement);
+requestAnimationFrame(function rAFloop() {
+  stats.update();
+  requestAnimationFrame(rAFloop);
+});
+
+// function getRandomColor() {
+//   const letters = "0123456789ABCDEF";
+//   let color = "#";
+//   for (var i = 0; i < 6; i++) {
+//     color += letters[Math.floor(Math.random() * 16)];
+//   }
+//   return color;
+// }
+
+// const __main__ = () => {
+//   const { createElement } = new LazyDOM();
+//   const p = createElement("p", {}, [
+//     createElement("h1", {}, ["Hello, World!"]),
+//     createElement("p", { class: "wow" }, ["This is just so... WOW!"]),
+//     createElement("p", { class: "wow2" }, ["Wow what happened! #2"]),
+//     createElement(
+//       "p",
+//       {
+//         class: "wow2",
+//         onDblclick: (e: Event) => {
+//           // @ts-ignore
+//           e.target.style.backgroundColor = getRandomColor();
+//         },
+//       },
+//       [
+//         "Wow what happened! #2 - This should be a cache hit! Inspect attributes.",
+//       ]
+//     ),
+//     createElement(
+//       "p",
+//       {
+//         class: "wow3",
+//         onClick: (e: Event) => {
+//           // @ts-ignore
+//           e.target.style.backgroundColor = getRandomColor();
+//         },
+//       },
+//       ["This is the least surprising message..."]
+//     ),
+//   ]);
+// };
+
+const MAX_SIZE = 100_000;
+
+const lazyDOMSuite = () => {
+  console.time("lazy");
+  console.warn("Running LazyDOM Suite.");
+  const instance = new LazyDOM();
+  const list = [];
+  let n = 0;
+  while (true) {
+    n++;
+    list.push(instance.createElement("div", {}, []));
+    if (n === MAX_SIZE) {
+      console.timeEnd("lazy");
+      console.log("total mem: " + console.memory.usedJSHeapSize.toPrecision(4));
+      break;
+    }
   }
-  return color;
-}
+};
 
-const instance = new LazyDOM();
-const p = instance.createElement("p", {}, [
-  instance.createElement("h1", {}, ["Hello, World!"]),
-  instance.createElement("p", { class: "wow" }, ["This is just so... WOW!"]),
-  instance.createElement("p", { class: "wow2" }, ["Wow what happened! #2"]),
-  instance.createElement(
-    "p",
-    {
-      class: "wow2",
-      onDblclick: (e: Event) => {
-        // @ts-ignore
-        e.target.style.backgroundColor = getRandomColor();
-      },
-    },
-    ["Wow what happened! #2 - This should be a cache hit! Inspect attributes."]
-  ),
-  instance.createElement(
-    "p",
-    {
-      class: "wow3",
-      onClick: (e: Event) => {
-        // @ts-ignore
-        e.target.style.backgroundColor = getRandomColor();
-      },
-    },
-    ["This is the least surprising message..."]
-  ),
-]);
+const regularDOMSuite = () => {
+  console.time("reg");
+  console.warn("Running Regular DOM Suite.");
+  const list = [];
+  let n = 0;
+  while (true) {
+    n++;
+    list.push(document.createElement("div"));
+    if (n === MAX_SIZE) {
+      console.timeEnd("reg");
+      console.log("total mem: " + console.memory.usedJSHeapSize.toPrecision(4));
+      break;
+    }
+  }
+};
 
-console.log(p);
-document.body.append(p);
+setTimeout(() => {
+  lazyDOMSuite();
+}, 1000);
+setTimeout(() => regularDOMSuite(), 5000);
+// = { createElement: console.log };
+// <p x="y">Hello, World! <span>wow</span></p>
